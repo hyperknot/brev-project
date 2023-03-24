e2d3c7e43ba284caa19f68faa3aea6bb1f2a5d03
Zsolt Ero <zsolt.ero@gmail.com>
2023-03-24 18:49:57 +0100

    project

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-touch brev-project-started
+touch "$HOME/brev-project-started"
 
 ####################################################################################
 ##### Specify software and dependencies that are required for this project     #####
@@ -72,3 +72,24 @@ touch brev-project-started
 ##### Custom commands #####
 # (echo ""; echo "##### Custom commands #####"; echo "";)
 # npm install
+
+### docker ###
+# # https://docs.docker.com/engine/install/ubuntu/
+# sudo apt-get install -y \
+#     ca-certificates \
+#     curl \
+#     gnupg \
+#     lsb-release
+# sudo mkdir -p /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
+# echo \
+#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# sudo apt-get update
+# sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# # https://docs.docker.com/engine/install/linux-postinstall/
+# sudo systemctl enable docker.service
+# sudo systemctl enable containerd.service
+# sudo usermod -aG docker $USER
+
+
